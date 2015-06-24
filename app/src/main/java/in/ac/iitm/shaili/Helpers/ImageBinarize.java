05f1a51baff8ada2418e5a8241f0c390d0e7a0bf
Awanish Raj <awanish@hyperverge.co>
2015-06-24 08:19:58 +0530

    Removing .idea/ from git

--- app/src/main/java/in/ac/iitm/shaili/Helpers/ImageBinarize.java
+++ app/src/main/java/in/ac/iitm/shaili/Helpers/ImageBinarize.java
@@ -43,6 +43,23 @@ public class ImageBinarize {
         return lum;
     }
 
+    public Bitmap toGrayscale(Bitmap bmpOriginal)
+    {
+        int width, height;
+        height = bmpOriginal.getHeight();
+        width = bmpOriginal.getWidth();
+
+        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmpGrayscale);
+        Paint paint = new Paint();
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        paint.setColorFilter(f);
+        c.drawBitmap(bmpOriginal, 0, 0, paint);
+        return bmpGrayscale;
+    }
+
     //Return histogram of the Image
     public static int[] mImageHistogram(Bitmap mImage) {
 
